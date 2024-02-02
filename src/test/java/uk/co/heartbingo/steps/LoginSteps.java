e8ecedb5df4bec26c605ff388e71054bd2558e5a
Krupali1994 <krupalicharlie2728@gmail.com>
2024-02-02 14:34:30 +0000

    heart bingo

--- src/test/java/uk/co/heartbingo/steps/LoginSteps.java
+++ src/test/java/uk/co/heartbingo/steps/LoginSteps.java
@@ -1,26 +1,25 @@
 package uk.co.heartbingo.steps;
 
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.testng.Assert;
-import uk.co.heartbingo.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+import static uk.co.heartbingo.browserfactory.ManageBrowser.driver;
 
 public class LoginSteps {
     @When("User clicks on Accept All Cookies")
     public void userClicksOnAcceptAllCookies() {
-        new LoginPage().clickOnCookiesButton();
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();",driver.findElement(By.id("onetrust-accept-btn-handler")));
     }
 
-    @And("User clicks on login link")
+    @Then("User clicks on login link")
     public void userClicksOnLoginLink() {
-        new LoginPage().clickOnLoginLink();
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();",driver.findElement(By.linkText("Log In")));
+
     }
 
-    @Then("User should navigate to login page successfully")
-    public void userShouldNavigateToLoginPageSuccessfully() {
-        String expectedMessage = "Log In to start playing.";
-        String actualMessage = new LoginPage().getLogInToStartPlayingText();
-        Assert.assertEquals(actualMessage, expectedMessage, "Login page not displayed");
-    }
+
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -2,7 +2,7 @@ Feature: Login Feature Functionality
 
   Background: User is on the HomePage
 
-  Scenario: User should navigate to login page successfully
+  Scenario: User should be able to click on Login button
     When User clicks on Accept All Cookies
-    And User clicks on login link
-    Then User should navigate to login page successfully
+    Then User clicks on login link
+
